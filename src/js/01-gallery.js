7e79f79286bf38f10eff8c4a80f1c6a1eaaafc1b
Demian <demyan.korotchenko@gmail.com>
2023-06-18 15:09:01 +0200

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
+//console.log(galleryItems);
 
+const galleryEls = document.querySelector('.gallery');
+
+const galleryMarkup = onCreateGalleryMarkup(galleryItems);
+
+galleryEls.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// rendered items
+function onCreateGalleryMarkup(items) {
+   return items.map(({ preview, original, description }) => {
+    return `<li>
+   <a class="gallery__item" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+   </a>
+ </li>`
+   }).join('');
+ }
+
+ // library
+    const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+ });
